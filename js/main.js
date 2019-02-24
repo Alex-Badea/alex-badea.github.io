d9ff47b78a04f76e4cfbc021966c197e25d27721
Alexandru-Octavian Badea <b.alexandruoctavian@gmail.com>
2019-02-24 17:42:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,12 +21,11 @@ Array.prototype.forEach.call(elements, function(element) {
   }
 });
 
-const Colors = Object.freeze({BLK:vec3.fromValues(0, 0, 0), BLU:vec3.fromValues(0, 0, 1), GRN:vec3.fromValues(0, 1, 0), CYN:vec3.fromValues(0, 1, 1), RED:vec3.fromValues(1, 0, 0), PNK:vec3.fromValues(1, 0, 1), YLW:vec3.fromValues(1, 1, 0), WHT:vec3.fromValues(1, 1, 1), D_BLU:vec3.fromValues(0, 0, 0.5), D_GRN:vec3.fromValues(0, 0.5, 0), D_CYN:vec3.fromValues(0, 0.5, 0.5), D_RED:vec3.fromValues(0.5, 0, 0), D_PNK:vec3.fromValues(0.5, 0, 0.5), D_YLW:vec3.fromValues(0.5, 0.5, 0), L_BLU:vec3.fromValues(0.5, 0.5, 1), L_GRN:vec3.fromValues(0.5, 1, 0.5), L_CYN:vec3.fromValues(0.5, 1, 1), L_RED:vec3.fromValues(1, 0.5, 0.5), L_PNK:vec3.fromValues(1, 0.5, 1), L_YLW:vec3.fromValues(1, 1, 0.5)});
 window.onload = () => {
 	const canvas = document.getElementById("glCanvas");
 	const gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
 	if (!gl) throw Error("Unable to initialize WebGL. Your browser or machine may not support it.");
 	const scene = new Scene(gl, {unlockRoll: true});
-	const World = new CoordSystem("World", [Colors.RED, Colors.GRN, Colors.BLU], mat4.create());
+	const World = new CoordSystem("World", [vec3.fromValues(1, 0, 0), vec3.fromValues(0, 1, 0), vec3.fromValues(0, 0, 1)], mat4.create());
 	scene.insert(World);
 }
