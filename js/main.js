989bb12a9bae1ec724705fcddd0f0623483e3807
Alexandru-Octavian Badea <b.alexandruoctavian@gmail.com>
2019-02-24 17:31:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ Array.prototype.forEach.call(elements, function(element) {
   }
 });
 
+const Colors = Object.freeze({BLK:vec3.fromValues(0, 0, 0), BLU:vec3.fromValues(0, 0, 1), GRN:vec3.fromValues(0, 1, 0), CYN:vec3.fromValues(0, 1, 1), RED:vec3.fromValues(1, 0, 0), PNK:vec3.fromValues(1, 0, 1), YLW:vec3.fromValues(1, 1, 0), WHT:vec3.fromValues(1, 1, 1), D_BLU:vec3.fromValues(0, 0, 0.5), D_GRN:vec3.fromValues(0, 0.5, 0), D_CYN:vec3.fromValues(0, 0.5, 0.5), D_RED:vec3.fromValues(0.5, 0, 0), D_PNK:vec3.fromValues(0.5, 0, 0.5), D_YLW:vec3.fromValues(0.5, 0.5, 0), L_BLU:vec3.fromValues(0.5, 0.5, 1), L_GRN:vec3.fromValues(0.5, 1, 0.5), L_CYN:vec3.fromValues(0.5, 1, 1), L_RED:vec3.fromValues(1, 0.5, 0.5), L_PNK:vec3.fromValues(1, 0.5, 1), L_YLW:vec3.fromValues(1, 1, 0.5)});
 window.onload = () => {
 	const canvas = document.getElementById("glCanvas");
 	const gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
