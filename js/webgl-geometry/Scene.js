7f3294d9fcedc8d8a395c41ed50bb7fab867ba78
Alexandru-Octavian Badea <b.alexandruoctavian@gmail.com>
2019-02-24 19:36:42 +0200

    Update Scene.js

--- js/webgl-geometry/Scene.js
+++ js/webgl-geometry/Scene.js
@@ -45,8 +45,8 @@ class Scene {
 				frontDir = vec3.normalize(vec3.create(), vec3.negate(vec3.create(), this.eyePos));
 				leftDir = vec3.normalize(vec3.create(), vec3.cross(vec3.create(), this.upDir, frontDir));
 				// Se va regenera upDir degenerat?
-				/*if (this.unlockRoll) 
-					this.upDir = vec3.normalize(vec3.create(), vec3.cross(vec3.create(), frontDir, leftDir));*/
+				if (this.unlockRoll) 
+					this.upDir = vec3.normalize(vec3.create(), vec3.cross(vec3.create(), frontDir, leftDir));
 			}
 			lastMousePos = vec2.fromValues(e.clientX, e.clientY);
 		};
