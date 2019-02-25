843dd9f224e610e65f8e2565f1b1a28c6f047080
Alexandru-Octavian Badea <b.alexandruoctavian@gmail.com>
2019-02-25 06:26:48 +0200

    Update worker_parseply.js

--- js/worker_parseply.js
+++ js/worker_parseply.js
@@ -1,4 +1,4 @@
-importScripts('js/webgl-geometry/misc/PlyModelLoader.js');
+importScripts('webgl-geometry/misc/PlyModelLoader.js');
 const meshLoader = new PlyModelLoader("https://raw.githubusercontent.com/Alex-Badea/alex-badea.github.io/master/js/mesh.ply", result => {
 	postMessage({positions: result.positions, normals: [], colors: result.colors, texInfo: result.texInfo, faces: result.faces});
 });
